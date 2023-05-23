0ce6e3a540395f239b6a5cdfc854f4ae9aac41eb
OperativeThunny <94724830+OperativeThunny@users.noreply.github.com>
2023-05-23 22:11:39 +0000

    Updated bash skeleton to have full explanation of the set options

--- useful_skeletons/bash_starter_skeleton.sh
+++ useful_skeletons/bash_starter_skeleton.sh
@@ -5,14 +5,45 @@
 #
 # Author: @OperativeThunny
 # Date: 12 May 2023
+# Updated: 23 May 2023
 ##
 # This script is licensed under the GNU Affero General Public License, version 3.0 (AGPLv3).
 #
 # A copy of the AGPLv3 license can be found at:
 # https://www.gnu.org/licenses/agpl-3.0.html
+##
+# See https://www.gnu.org/software/bash/manual/html_node/The-Set-Builtin.html for an explanation of `set` options.
+#
+# -E If set, any trap on ERR is inherited by shell functions, command 
+#    substitutions, and commands executed in a subshell environment. The ERR 
+#    trap is normally not inherited in such cases.
+# -e Exit immediately if a pipeline (see Pipelines), which may consist of a single simple command, a list, 
+#    or a compound command returns a non-zero status. See the documentation for a much larger explanation.
+# -u Treat unset variables and parameters other than the special parameters ‘@’ or ‘*’, or array variables subscripted
+#    with ‘@’ or ‘*’, as an error when performing parameter expansion. An error message will be written to the standard 
+#    error, and a non-interactive shell will exit.
+# -o option-name
+#    Set the option corresponding to option-name: 
+#   errexit Same as -e.
+#   errtrace Same as -E.
+#   nounset Same as -u.
+#   pipefail
+#       If set, the return value of a pipeline is the value of the last (rightmost) command to exit with a non-zero 
+#       status, or zero if all commands in the pipeline exit successfully. This option is disabled by default.
+# 
+#  The current set of options may be found in $-.
 
-# Set options for improved error handling
-set -Eeou pipefail
+# Set options for improved error handling:
+set -Eeuo pipefail
+
+# Debugging options:
+# -v Print shell input lines as they are read.
+# -x Print a trace of simple commands, for commands, case commands, select commands, and arithmetic for commands and
+#    their arguments or associated word lists after they are expanded and before they are executed. The value of the 
+#    PS4 variable is expanded and the resultant value is printed before the command and its expanded arguments.
+#set -xv # Uncomment for debugging.
+# -n Read commands but do not execute them. This may be used to check a script for syntax errors. This option is ignored by interactive shells.
+#set -n
 
 # Set variables
 export PROG_NAME="Program_or_script_name_goes_here";
